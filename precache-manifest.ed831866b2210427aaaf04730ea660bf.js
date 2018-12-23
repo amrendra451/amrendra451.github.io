65b7cef12869fb6630b65d9e9b81c6b0e06c9027
amrendra451 <cool.amrendrasrivastav@gmail.com>
2018-12-23 23:18:55 +0530

    initial commit

---
+++ precache-manifest.ed831866b2210427aaaf04730ea660bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1812d6ccccc1c651199",
+    "url": "/static/js/main.b1812d6c.chunk.js"
+  },
+  {
+    "revision": "c9b2afa0f82ca7a41599",
+    "url": "/static/js/1.c9b2afa0.chunk.js"
+  },
+  {
+    "revision": "b1812d6ccccc1c651199",
+    "url": "/static/css/main.904a0779.chunk.css"
+  },
+  {
+    "revision": "c9b2afa0f82ca7a41599",
+    "url": "/static/css/1.b498d1f8.chunk.css"
+  },
+  {
+    "revision": "a8bc3393ac38185702bbff3c2d1df4a9",
+    "url": "/index.html"
+  }
+];
